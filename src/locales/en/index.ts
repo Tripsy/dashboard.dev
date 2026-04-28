69fc5f43664ed72a5010fc1d2f35cf96e794ffe4
Tripsy <engine@play-zone.ro>
2026-04-28 03:34:05 +0300

    json renamed

--- src/locales/en/index.ts
+++ src/locales/en/index.ts
@@ -1,33 +1,34 @@
-import account_delete from '@/locales/en/account_delete.json';
-import account_edit from '@/locales/en/account_edit.json';
-import account_email_update from '@/locales/en/account_email_update.json';
-import account_me from '@/locales/en/account_me.json';
-import account_password_update from '@/locales/en/account_password_update.json';
+import account_delete from '@/locales/en/account-delete.json';
+import account_edit from '@/locales/en/account-edit.json';
+import account_email_update from '@/locales/en/account-email-update.json';
+import account_me from '@/locales/en/account-me.json';
+import account_password_update from '@/locales/en/account-password-update.json';
 import app from '@/locales/en/app.json';
 import auth from '@/locales/en/auth.json';
 import brands from '@/locales/en/brands.json';
-import cash_flow from '@/locales/en/cash_flow.json';
-import client_address from '@/locales/en/client_address.json';
+import cash_flow from '@/locales/en/cash-flow.json';
+import client_address from '@/locales/en/client-address.json';
 import clients from '@/locales/en/clients.json';
-import company_vehicles from '@/locales/en/company_vehicles.json';
-import cron_history from '@/locales/en/cron_history.json';
+import company_vehicles from '@/locales/en/company-vehicles.json';
+import cron_history from '@/locales/en/cron-history.json';
 import dashboard from '@/locales/en/dashboard.json';
-import email_confirm from '@/locales/en/email_confirm.json';
-import email_confirm_send from '@/locales/en/email_confirm_send.json';
+import email_confirm from '@/locales/en/email-confirm.json';
+import email_confirm_send from '@/locales/en/email-confirm-send.json';
 import home from '@/locales/en/home.json';
-import log_data from '@/locales/en/log_data.json';
-import log_history from '@/locales/en/log_history.json';
+import log_data from '@/locales/en/log-data.json';
+import log_history from '@/locales/en/log-history.json';
 import login from '@/locales/en/login.json';
 import logout from '@/locales/en/logout.json';
-import mail_queue from '@/locales/en/mail_queue.json';
-import password_recover from '@/locales/en/password_recover.json';
-import password_recover_change from '@/locales/en/password_recover_change.json';
+import mail_queue from '@/locales/en/mail-queue.json';
+import password_recover from '@/locales/en/password-recover.json';
+import password_recover_change from '@/locales/en/password-recover-change.json';
 import permissions from '@/locales/en/permissions.json';
 import places from '@/locales/en/places.json';
 import register from '@/locales/en/register.json';
 import templates from '@/locales/en/templates.json';
 import users from '@/locales/en/users.json';
 import vehicles from '@/locales/en/vehicles.json';
+import work_sessions from '@/locales/en/work-sessions.json';
 
 const en = {
 	'account-delete': account_delete,
@@ -61,6 +62,7 @@ const en = {
 
 	vehicles,
 	'company-vehicles': company_vehicles,
+	'work-sessions': work_sessions,
 };
 
 export default en;
